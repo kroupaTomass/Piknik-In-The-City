5994dc191b61ccc1946be1e5cf89f3253a8ccefd
kroupaTomass <tomas.kroupa@cadstudio.cz>
2015-10-15 09:48:15 +0200

    Update facebook plugin version to v2.5

--- app/scripts/components/panelFacebook.js
+++ app/scripts/components/panelFacebook.js
@@ -14,10 +14,11 @@
     render() {
       return (
         <div className='div-facebook-panel' data-sr="move 16px scale down 20%, over 2s">
-          <div className="fb-page" data-href="http://www.facebook.com/PiknikintheCity" data-width="800" data-small-header="false"
-          data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-show-posts="true">
-          <div className="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/PiknikintheCity">
-          <a href="https://www.facebook.com/PiknikintheCity">Piknik in the City</a></blockquote></div></div>
+          <div className="fb-page" data-href="https://www.facebook.com/PiknikintheCity" data-width="500" data-small-header="false" data-adapt-container-width="true"
+          data-hide-cover="false" data-show-facepile="true" data-show-posts="true"><div className="fb-xfbml-parse-ignore">
+            <blockquote cite="https://www.facebook.com/PiknikintheCity">
+            <a href="https://www.facebook.com/PiknikintheCity">Piknik in the City</a></blockquote></div>
+          </div>
         </div>
       );
     }
